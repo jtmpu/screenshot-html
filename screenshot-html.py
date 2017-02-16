efb2a0c58cac8b7a93387108b7a022b392bb3f1a
jv <jv>
2017-02-16 21:07:30 +0100

    Added threads and changed user agent.

--- screenshot-html.py
+++ screenshot-html.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import subprocess
+import threading
 import argparse
 import requests
 import sys
@@ -28,21 +29,67 @@ def main(args):
     phantomjs_script_path = "/tmp/phantomjs_bootstrap.js"
     setup_base(args, phantomjs_script_path)
     urls = setup_urls(args["urls"]) 
+    thread_count = args["threads"]
+    url_count = len(urls)
+    urls_per_shard = url_count / thread_count
+
+    shards = []
+    for i in range(0, thread_count):
+        start = i * urls_per_shard
+        end = start + urls_per_shard
+        if i == thread_count - 1:
+            end = url_count
+        shards.append( (start, end) )
 
     results = {}
-    for url in urls:
-        if verbose_enabled:
-            print("[+] Fetching %s" % url)
-        results[url] = {}
-        result_path = os.path.abspath(args["basefolder"] + "/" + url_to_filename(url) + ".png")
-        process = subprocess.Popen(["phantomjs", phantomjs_script_path, url, result_path], stdout=subprocess.PIPE)
-        line = process.stdout.readline().strip()
-        results[url]["path"] = result_path
-        results[url]["status"] = line
 
-        if verbose_enabled:
-            print("[+] Done: %s " % line)
+    loglock = threading.Lock()
+    threads = []
+    for i in range(0, thread_count):
+        shard = shards[i]
+        threads.append(ThreadedDownloader(urls, args["basefolder"], phantomjs_script_path, verbose_enabled, shard[0], shard[1], loglock))
+    
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        try:
+            thread.join()
+        except:
+            continue
+
+    for thread in threads:
+        results.update(thread.results)
+
     return results
+
+class ThreadedDownloader(threading.Thread):
+    def __init__(self, urls, basefolder, phantomjs_script, verbose_enabled, start, end, loglock):
+        threading.Thread.__init__(self)
+        self.urls = urls
+        self.verbose = verbose_enabled
+        self.basefolder = basefolder
+        self.phantomjs_script = phantomjs_script
+        self.start_index = start
+        self.end_index = end
+        self.loglock = loglock
+        self.results = {}
+
+    def run(self):
+        self.results = {}
+        for i in range(self.start_index, self.end_index):
+            url = self.urls[i]
+            self.results[url] = {}
+            result_path = os.path.abspath(self.basefolder + "/" + url_to_filename(url) + ".png")
+            process = subprocess.Popen(["phantomjs", self.phantomjs_script, url, result_path], stdout=subprocess.PIPE)
+            line = process.stdout.readline().strip()
+            self.results[url]["path"] = result_path
+            self.results[url]["status"] = line
+
+            if self.verbose:
+                self.loglock.acquire()
+                print("[+] Result: %s (%s)" % (line, url))
+                self.loglock.release()
 
 def plugin_run(args):
     '''
@@ -59,6 +106,7 @@ def parse_cmdline():
     parser.add_argument("-f", "--format", help="The format to use for the output", choices=["json", "grep"], default="grep")
     parser.add_argument("-b", "--basefolder", help="The folder to place the screenshots in.", default="./screenshots")
     parser.add_argument("-v", "--verbose", help="Outputs the status of the script.", action="store_true")
+    parser.add_argument("-t", "--threads", help="Amount of threads to use", default=1, type=int)
     args = parser.parse_args()
     
     if args.read:
@@ -77,6 +125,7 @@ def setup_base(args, payload_path):
     '''
     javascript_payload = """
 var page = require("webpage").create();
+page.settings.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/55.0.2883.87 Chrome/55.0.2883.87 Safari/537.36";
 var system = require("system");
 var args = system.args;
 page.open(args[1], function(status) {
@@ -109,4 +158,3 @@ def setup_urls(urls):
 if __name__ == "__main__":
     args = parse_cmdline()
     results = main(vars(args))
-    print("CLI Done.")
