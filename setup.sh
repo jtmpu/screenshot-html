21a6f7b93fbf0ecfc40734958a27f4b5396a4c84
jv <jv>
2017-02-16 17:21:11 +0100

    Updated and fixed setup.

--- setup.sh
+++ setup.sh
@@ -3,5 +3,9 @@
 echo "Installing the following software:"
 echo "1) phantomjs"
 echo ""
-echo ""
 sudo apt install phantomjs
+
+echo "Done"
+echo ""
+echo "Setting up bin links."
+sudo ln -s $(pwd)/screenshot-html.py /usr/bin/screenshot-html
